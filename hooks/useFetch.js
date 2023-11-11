1c2330c1b31e48af2138ce0244d1f00708e3ceae
Akira <junrongfu6@gmail.com>
2023-11-11 15:03:08 +0800

    home screen layout

--- hooks/useFetch.js
+++ hooks/useFetch.js
@@ -6,6 +6,27 @@ const useFetch =()=>{
     const [isLoading, setIsLoading] = useState(false)
     const [error, setError] = useState(null)
 
+    const options = {
+        method: 'GET',
+        url: `https://jsearch.p.rapidapi.com/search`,
+        params: {
+          query: 'Python developer in Texas, USA',
+          page: '1',
+          num_pages: '1'
+        },
+        headers: {
+          'X-RapidAPI-Key': '6cea82154dmshc756303655c2ac3p1b99d6jsn7150a2b9bf82',
+          'X-RapidAPI-Host': 'jsearch.p.rapidapi.com'
+        }
+      };
+      
+      try {
+          const response = await axios.request(options);
+          console.log(response.data);
+      } catch (error) {
+          console.error(error);
+      }
+
 }
 
 export default useFetch
