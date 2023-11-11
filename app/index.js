6622e805289a91f14b87940a5c28c77c8e88a05e
Akira <junrongfu6@gmail.com>
2023-11-11 11:58:01 +0800

    first commit

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Happy birthday, Bevan!
+            </Text>
+        </View>
+        )
+}
+
+export default Home
